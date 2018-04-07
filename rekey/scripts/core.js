29c780f8fd05a1f1af49782431e18b02d00b065d
mnemoto <mnemoto.dev@gmail.com>
2018-04-07 20:58:37 +0900

    Update: added consideration for release triggering on Key-up event

--- rekey/scripts/core.js
+++ rekey/scripts/core.js
@@ -1,22 +1,31 @@
 let __rekey_intrinsics__ = {
-    remapRules: []
+    /** @type [RemapRule] */    remapRules: [],
+    /** @type Set<RemapRule> */    triggeredRules: new Set(),
+    /** @type Set<number> */    pressedKeys: new Set()
 };
 
 class RemapRule {
-    constructor(predict, remapped) {
+    /**
+     * @param predict {InOut|number}
+     * @param remapped {InOut|function(Context)|number}
+     * @param releaseFunc {(InOut|function(Context))?}
+     */
+    constructor(predict, remapped, releaseFunc) {
         this.predict = predict;
         this.remapped = remapped;
+        this.releaseFunc = releaseFunc;
     }
 }
 
 /**
  *
- * @param predict
- * @param remapped {function(Context)}
+ * @param predict {InOut|number}
+ * @param remapped {InOut|function(Context)|number}
+ * @param releaseFunc {(InOut|function(Context)|number)?}
  */
-function addRemap(predict, remapped) {
+function addRemap(predict, remapped, releaseFunc) {
     __rekey_intrinsics__.remapRules.push(
-        new RemapRule(predict, remapped)
+        new RemapRule(predict, remapped, releaseFunc)
     );
 }
 
@@ -28,66 +37,60 @@ function getKeyName(keyCode) {
     return name
 }
 
-function getKeyCode(keyName) {
-    const code = KeyCodes[keyName];
-    if (code === undefined) {
-        console.log(`Unknown KeyName: ${keyName}`);
-    }
-    return code;
-}
-
-/*
-
-example of remapping A => B:
-
-  addRemap(e=>e.keyCode==getKeyCode('A'), e=>{
-    e.keyCode=getKeyCode('B')
-    Key.emit(e)
-  }
-
-  addRemap('A','B')
-
-example of remapping Alt + A => B:
-
-  addRemap(e=>e.isAlt && e.keyCode==getKeyCode('A'), e=>{
-    e.keyCode=getKeyCode('B')
-    Key.emit(e)
-  }
-
-  addRemap('A-A','B')
-
-example of remapping Alt + A => B :
-
-  addRemap(e=>e.isAlt && e.keyCode==getKeyCode('A'), e=>{
-    e.keyCode=getKeyCode('B')
-    Key.emit(e)
-  }
-
-  addRemap('A-A','B')
-*/
-
+/** @param ctx {Context}
+ *  @param remapped {Function|number} */
 function executeRemapAction(ctx, remapped) {
-    if (remapped instanceof InOut) {
-        remapped.output(ctx);
-        return
-    }
-    switch (typeof (remapped)) {
-        case 'function':
-            remapped(ctx);
+    let newCtx = new Context(ctx.keyCode, 0, ctx.isRepeat, ctx.isUp, ctx.isSysKey, ctx.keyboardType);
+    let next = remapped;
+
+    while (next instanceof InOut)
+        next = next.output(newCtx);
+
+    if (typeof (next) === 'function')
+        next(newCtx);
+    else if (typeof (next) === 'number') {
+        // small flags fix according to next flags
+        {
+            newCtx.flags |= 256;
+            // noinspection JSBitwiseOperatorUsage
+            if (newCtx.flags & Masks.LSHIFT) newCtx.flags |= Masks.SHIFT_MASK;
+            // noinspection JSBitwiseOperatorUsage
+            if (newCtx.flags & Masks.LCONTROL) newCtx.flags |= Masks.CTRL_MASK;
+        }
+
+        // change flags if current flags differs from next
+        if (ctx.flags !== newCtx.flags) Key.emitFlagsChange({flags: newCtx.flags});
+
+
+        Key.emit(next, newCtx);
+        // get flags back to before
+        if (ctx.flags !== newCtx.flags) Key.emitFlagsChange({flags: ctx.flags});
+    } else {
+        console.log(`unknown type ${typeof (next)}`);
     }
 }
 
 const Masks = {
-    LCONTROL: 1<<0,
-    LSHIFT: 1<<1,
-    RSHIFT: 1<<2,
-    LCOMMAND: 1<<3,
-    RCOMMAND: 1<<4,
-    LALT: 1<<5
+    CTRL_MASK: 1 << 18,
+    LCONTROL: 1 << 0,
+    SHIFT_MASK: 1 << 17,
+    LSHIFT: 1 << 1,
+    RSHIFT: 1 << 2,
+    LCOMMAND: 1 << 3,
+    RCOMMAND: 1 << 4,
+    LALT: 1 << 5
 };
 
 class Context {
 
+    /**
+     * @param keyCode number
+     * @param flags integer
+     * @param isRepeat boolean
+     * @param isUp boolean
+     * @param isSysKey boolean
+     * @param keyboardType integer
+     */
     constructor(keyCode,
                 flags,
                 isRepeat,
@@ -106,201 +109,221 @@ class Context {
         return (this.flags & Masks.LSHIFT) !== 0;
     }
 
-    get isLeftControlPressed(){
+    get isLeftControlPressed() {
         return (this.flags & Masks.LCONTROL) !== 0;
     }
 
 }
 
+/**
+ * @param ctx {Context}
+ * @param remapRules {[RemapRule]}
+ * @returns RemapRule
+ */
+function findMatchedRule(ctx, remapRules) {
+    for (let remapRule of remapRules) {
+        const predictVal = remapRule.predict;
+        if (predictVal instanceof InOut) {
+            if (match(ctx, predictVal))
+                return remapRule;
+        } else if (typeof (predictVal) === 'number') {
+            if (predictVal === ctx.keyCode)
+                return remapRule;
+        } else if (typeof (predictVal) === 'string') {
+            if (predictVal.toLowerCase() === getKeyName(ctx.keyCode).toLowerCase())
+                return remapRule;
+        } else if (typeof (predictVal) === 'function') {
+            if (predictVal(ctx))
+                return remapRule;
+        } else if (Array.isArray(predictVal))
+            for (let pred of predictVal)
+                if (findMatchedRule(ctx, {
+                    predict: pred,
+                    remapped: remapRule.remapped
+                }))
+                    return remapRule;
+    }
+}
 
 /**
  * @param ctx {Context}
  * @param remapRules {[RemapRule]}
- * @returns {boolean}
+ * @returns {RemapRule}
  */
 function acquireRemapAction(ctx, remapRules) {
-    for (let remapRule of remapRules) {
-        let predictVal = remapRule.predict;
-        let remapped = remapRule.remapped;
-
-        if (predictVal instanceof InOut) {
-            if (match(ctx, predictVal)) {
-                executeRemapAction(ctx, remapped);
-                return true;
-            }
-            continue;
-        }
-
-        // remapRules.forEach((predict, remapped) => {
-        switch (typeof (predictVal)) {
-            case 'number':
-                if (predictVal === ctx.keyCode) {
-                    executeRemapAction(ctx,remapped);
-                    return true
-                }
-                break;
-            case 'string':
-                if (predictVal.toLowerCase() === getKeyName(ctx.keyCode).toLowerCase()) {
-                    executeRemapAction(ctx,remapped);
-                    return true
-                }
-                break;
-            case 'function':
-                if (predictVal(ctx)) {
-                    executeRemapAction(ctx,remapped);
-                    return true
-                }
-                break;
-            default:
-                if (Array.isArray(predictVal)) {
-                    for (let pred of predictVal) {
-                        if (acquireRemapAction(ctx, {
-                            predict: pred,
-                            remapped: remapped
-                        })) {
-                            return true
-                        }
-                    }
-                    return true
-                }
-        }
+    const matchedRemapRule = findMatchedRule(ctx, remapRules);
+    if (matchedRemapRule) {
+        __rekey_intrinsics__.triggeredRules.add(matchedRemapRule);
+        executeRemapAction(ctx, matchedRemapRule.remapped);
+        return matchedRemapRule
     }
+}
+
+// noinspection JSUnusedGlobalSymbols
+function onFlagsChanged(key, flags, isRepeat, isUp, isSysKey, keyboardType) {
+    Key.emitFlagsChange({flags: flags, keyboardType: keyboardType})
 }
 
 // noinspection JSUnusedGlobalSymbols
 function onKey(keyCode, flags, isRepeat, isUp, isSysKey, keyboardType) {
     let ctx = new Context(keyCode, flags, isRepeat, isUp, isSysKey, keyboardType);
-    if (!acquireRemapAction(
-        ctx,
-        __rekey_intrinsics__.remapRules)) {
-        Key.emit(keyCode, {
-            isUp: isUp,
-            keyboardType: keyboardType
+    if (!isUp) {
+        __rekey_intrinsics__.pressedKeys.add(keyCode);
+        const matchedRemapRule = acquireRemapAction(ctx, __rekey_intrinsics__.remapRules);
+        if (!matchedRemapRule) Key.emit(keyCode, {isUp: isUp, keyboardType: keyboardType});
+    } else {
+        __rekey_intrinsics__.pressedKeys.delete(keyCode);
+        const releaseTargets = Array.from(__rekey_intrinsics__.triggeredRules).filter(rule => {
+                let next = rule.predict;
+                while (next instanceof InOut) {
+                    if (!next.predict(ctx)) return true;
+                    next = next.next
+                }
+                if (typeof(next) === 'number') {
+                    return !__rekey_intrinsics__.pressedKeys.has(next)
+                }
+            }
+        );
+
+        releaseTargets.forEach(rule => {
+            if (rule.releaseFunc) rule.releaseFunc(ctx);
+            else {
+                executeRemapAction(ctx, rule.remapped);
+                __rekey_intrinsics__.triggeredRules.delete(rule);
+            }
         });
+
+        if (releaseTargets.length === 0) {
+            Key.emit(keyCode, ctx);
+        }
+
     }
 }
 
-
 const KeyCodes = {
-    Enter: 0x24,
-    tab: 0x30,
-    space: 0x31,
-    delete: 0x33,
-    escape: 0x35,
-    command: 0x37,
-    shift: 0x38,
-    capsLock: 0x39,
-    option: 0x3A,
-    control: 0x3B,
-    rightShift: 0x3C,
-    rightOption: 0x3D,
-    rightControl: 0x3E,
-    leftArrow: 0x7B,
-    rightArrow: 0x7C,
-    downArrow: 0x7D,
-    upArrow: 0x7E,
-    volumeUp: 0x48,
-    volumeDown: 0x49,
-    mute: 0x4A,
-    help: 0x72,
-    home: 0x73,
-    pageUp: 0x74,
-    forwardDelete: 0x75,
-    end: 0x77,
-    pageDown: 0x79,
-    function: 0x3F,
-    f1: 0x7A,
-    f2: 0x78,
-    f4: 0x76,
-    f5: 0x60,
-    f6: 0x61,
-    f7: 0x62,
-    f3: 0x63,
-    f8: 0x64,
-    f9: 0x65,
-    f10: 0x6D,
-    f11: 0x67,
-    f12: 0x6F,
-    f13: 0x69,
-    f14: 0x6B,
-    f15: 0x71,
-    f16: 0x6A,
-    f17: 0x40,
-    f18: 0x4F,
-    f19: 0x50,
-    f20: 0x5A,
+    Enter: 36,
+    Tab: 48,
+    Space: 49,
+    Delete: 51,
+    Escape: 53,
+    Command: 55,
+    Shift: 56,
+    CapsLock: 57,
+    Option: 58,
 
-    // US-ANSI Keyboard Positions,
-    // eg. These key codes are for the physical key (in any keyboard layout),
-    // at the location of the named key in the US-ANSI layout.,
-    a: 0x00,
-    b: 0x0B,
-    c: 0x08,
-    d: 0x02,
-    e: 0x0E,
-    f: 0x03,
-    g: 0x05,
-    h: 0x04,
-    i: 0x22,
-    j: 0x26,
-    k: 0x28,
-    l: 0x25,
-    m: 0x2E,
-    n: 0x2D,
-    o: 0x1F,
-    p: 0x23,
-    q: 0x0C,
-    r: 0x0F,
-    s: 0x01,
-    t: 0x11,
-    u: 0x20,
-    v: 0x09,
-    w: 0x0D,
-    x: 0x07,
-    y: 0x10,
-    z: 0x06,
+    Control: 59,
+    RightShift: 60,
+    RightOption: 61,
+    RightControl: 62,
 
-    zero: 0x1D,
-    one: 0x12,
-    two: 0x13,
-    three: 0x14,
-    four: 0x15,
-    five: 0x17,
-    six: 0x16,
-    seven: 0x1A,
-    eight: 0x1C,
-    nine: 0x19,
+    LeftArrow: 123,
+    RightArrow: 124,
+    DownArrow: 125,
+    UpArrow: 126,
 
-    equals: 0x18,
-    minus: 0x1B,
-    semicolon: 0x29,
-    apostrophe: 0x27,
-    comma: 0x2B,
-    period: 0x2F,
-    forwardSlash: 0x2C,
-    backslash: 0x2A,
-    grave: 0x32,
-    leftBracket: 0x21,
-    rightBracket: 0x1E,
+    VolumeUp: 72,
+    VolumeDown: 73,
+    Mute: 74,
+    Help: 114,
 
-    keypadDecimal: 0x41,
-    keypadMultiply: 0x43,
-    keypadPlus: 0x45,
-    keypadClear: 0x47,
-    keypadDivide: 0x4B,
-    keypadEnter: 0x4C,
-    keypadMinus: 0x4E,
-    keypadEquals: 0x51,
-    keypad0: 0x52,
-    keypad1: 0x53,
-    keypad2: 0x54,
-    keypad3: 0x55,
-    keypad4: 0x56,
-    keypad5: 0x57,
-    keypad6: 0x58,
-    keypad7: 0x59,
-    keypad8: 0x5B,
-    keypad9: 0x5C
+    Home: 115,
+    PageUp: 116,
+    ForwardDelete: 117,
+    End: 119,
+    PageDown: 121,
+
+    Function: 63,
+
+    F1: 122,
+    F2: 120,
+    F4: 118,
+    F5: 96,
+    F6: 97,
+    F7: 98,
+    F3: 99,
+    F8: 100,
+    F9: 101,
+    F10: 109,
+    F11: 103,
+    F12: 111,
+    F13: 105,
+    F14: 107,
+    F15: 113,
+    F16: 106,
+    F17: 64,
+    F18: 79,
+    F19: 80,
+    F20: 90,
+
+    A: 0,
+    B: 11,
+    C: 8,
+    D: 2,
+    E: 14,
+    F: 3,
+    G: 5,
+    H: 4,
+    I: 34,
+    J: 38,
+    K: 40,
+    L: 37,
+    M: 46,
+    N: 45,
+    O: 31,
+    P: 35,
+    Q: 12,
+    R: 15,
+    S: 1,
+    T: 17,
+    U: 32,
+    V: 9,
+    W: 13,
+    X: 7,
+    Y: 16,
+    Z: 6,
+
+    _0: 29,
+    _1: 18,
+    _2: 19,
+    _3: 20,
+    _4: 21,
+    _5: 23,
+    _6: 22,
+    _7: 26,
+    _8: 28,
+    _9: 25,
+
+    Equals: 24,
+    Minus: 27,
+    Semicolon: 41,
+    Apostrophe: 39,
+    Comma: 43,
+    Period: 47,
+    ForwardSlash: 44,
+    Backslash: 42,
+    Grave: 50,
+    LeftBracket: 33,
+    RightBracket: 30,
+
+    KeypadDecimal: 65,
+    KeypadMultiply: 67,
+    KeypadPlus: 69,
+    KeypadClear: 71,
+    KeypadDivide: 75,
+    KeypadEnter: 76,
+    KeypadMinus: 78,
+    KeypadEquals: 81,
+
+    Keypad0: 82,
+    Keypad1: 83,
+    Keypad2: 84,
+    Keypad3: 85,
+    Keypad4: 86,
+    Keypad5: 87,
+    Keypad6: 88,
+    Keypad7: 89,
+    Keypad8: 91,
+    Keypad9: 92
 };
 
 const KeyCodeToKeyName = Object.keys(KeyCodes).reduce((acc, key) => {
@@ -330,51 +353,47 @@ function match(ctx, predict) {
 
 class InOut {
     /**
-     * @param next {InOut|string}
-     * @param predict {function(Context)}
-     * @param output {function(Context)}
+     * @param next {InOut|string|number}
+     * @param predict {Function|number|InOut}
+     * @param output {(Function|number|InOut)?}
+     * @param release {(Function|InOut)?}
      */
-    constructor(next, predict, output) {
+    constructor(next, predict, output, release) {
         this.next = next;
         this.predict = predict;
-        this.output = output;
+        this.output = ctx => {
+            output(ctx);
+            return this.next;
+        };
+        this.release = release;
     }
 }
 
-let emitNext = (ctx, nextValue) => {
-    if (nextValue instanceof InOut) {
-        nextValue.output(ctx);
-        return;
-    }
-    switch (typeof(nextValue)) {
-        case "string":
-            Key.emit(getKeyCode(nextValue));
-            break;
-        case "number":
-            Key.emit(nextValue);
-            break;
-        default:
-            console.log(typeof(nextValue))
-    }
-}
+let Control = next => new InOut(next,
+    ctx => ctx.isLeftControlPressed,
+    ctx => !ctx.isUp ? (ctx.flags |= Masks.LCONTROL) : (ctx.flags &= !Masks.LCONTROL)
+);
 
-let Control = next => {
-    return new InOut(
-        next,
-        ctx => ctx.isLeftControlPressed,
-        ctx => {
-            if (!ctx.isLeftControlPressed) {
-                Key.emitFlagsChange({flags: ctx.flags | Masks.LCONTROL, keyboardType: ctx.keyboardType});
-            }
-            emitNext(ctx, next);
-        });
-};
+let Shift = (next) => new InOut(next,
+    ctx => ctx.isLeftShiftPressed,
+    ctx => ctx.flags |= Masks.LSHIFT
+);
 
-// addRemap(Control(KeyCodes.e), Control(KeyCodes.a));
+let Up = (next) => new InOut(next,
+    ctx => ctx.isUp,
+    ctx => ctx.isUp = true,
+    ctx => ctx.isUp
+);
 
 Mouse.setAttenuation(11);
-addRemap(Control(KeyCodes.e), ctx => Mouse.setForce({y: ctx.isUp ? 0 : -12}));
-addRemap(Control(KeyCodes.d), ctx => Mouse.setForce({y: ctx.isUp ? 0 : 12}));
-addRemap(Control(KeyCodes.s), ctx => Mouse.setForce({x: ctx.isUp ? 0 : -12}));
-addRemap(Control(KeyCodes.f), ctx => Mouse.setForce({x: ctx.isUp ? 0 : 12}));
-addRemap(Control(KeyCodes.g), Control(KeyCodes.a));
+
+addRemap(Control(KeyCodes.E),
+    ctx => Mouse.setForce({y: ctx.isUp ? 0 : -12}),
+    ctx => Mouse.setForce({y: 0})
+);
+// addRemap(Control(KeyCodes.D), ctx => Mouse.setForce({y: ctx.isUp ? 0 : 12}));
+// addRemap(Control(KeyCodes.S), ctx => Mouse.setForce({x: ctx.isUp ? 0 : -12}));
+// addRemap(Control(KeyCodes.F), ctx => Mouse.setForce({x: ctx.isUp ? 0 : 12}));
+addRemap(Control(KeyCodes.G), Shift(KeyCodes.A));
+addRemap(Shift(KeyCodes.F), Control(KeyCodes.A));
+addRemap(Control(KeyCodes._1), Shift(KeyCodes._2));
