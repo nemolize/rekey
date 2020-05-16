df9559e2ff8cf094a1a478058176807841cd45f9
nemolize <nemolize@users.noreply.github.com>
2020-05-16 17:16:31 +0900

    feat: adds save/load

--- rekey/ViewController.swift
+++ rekey/ViewController.swift
@@ -9,6 +9,8 @@ import Foundation
 import RxCocoa
 import RxSwift
 
+let configPathRelativeToHome = ".config/rekey/config.json"
+
 class ViewController: NSViewController, NSTextViewDelegate {
     @IBOutlet weak var upButton: NSButton!
     @IBOutlet weak var downButton: NSButton!
@@ -21,12 +23,16 @@ class ViewController: NSViewController, NSTextViewDelegate {
         super.viewDidLoad()
         subscribeHotKeys()
         subscribeButtons()
+        loadConfig()
         updateLabels()
     }
 
     private func subscribeHotKeys() {
         WindowMoveHotKeyService.shared.onChangeHotKey
-            .subscribe({ _ in self.updateLabels() })
+            .subscribe({ _ in
+                self.updateLabels()
+                self.saveConfig()
+            })
             .disposed(by: disposeBag)
     }
 
@@ -54,6 +60,59 @@ class ViewController: NSViewController, NSTextViewDelegate {
         self.downButton.title = getHotKeyLabel(.down)
         self.leftButton.title = getHotKeyLabel(.left)
         self.rightButton.title = getHotKeyLabel(.right)
+    }
+
+    private func loadConfig() {
+        do {
+            let data = try Data(contentsOf: configPath, options: .mappedIfSafe)
+            guard let json = try JSONSerialization.jsonObject(
+                with: data, options: .mutableLeaves) as? [String: Any] else {
+                debugPrint("content of config is empty")
+                return
+            }
+            if let windowMove = json["windowMove"] as? [String: Any] {
+                if let up = windowMove["up"] as? [String: Any] {
+                    WindowMoveHotKeyService.shared.setHotKey(direction: .up, dictionary: up)
+                }
+                if let down = windowMove["down"] as? [String: Any] {
+                    WindowMoveHotKeyService.shared.setHotKey(direction: .down, dictionary: down)
+                }
+                if let left = windowMove["left"] as? [String: Any] {
+                    WindowMoveHotKeyService.shared.setHotKey(direction: .left, dictionary: left)
+                }
+                if let right = windowMove["right"] as? [String: Any] {
+                    WindowMoveHotKeyService.shared.setHotKey(direction: .right, dictionary: right)
+                }
+            }
+
+        } catch {
+            debugPrint(error)
+        }
+    }
+
+    private func saveConfig() {
+        let dict: [String: Any?] = [
+            "windowMove": [
+                "up": WindowMoveHotKeyService.shared.getHotKey(.up)?.keyCombo.dictionary,
+                "down": WindowMoveHotKeyService.shared.getHotKey(.down)?.keyCombo.dictionary,
+                "left": WindowMoveHotKeyService.shared.getHotKey(.left)?.keyCombo.dictionary,
+                "right": WindowMoveHotKeyService.shared.getHotKey(.right)?.keyCombo.dictionary
+            ]
+        ]
+
+        do {
+            let data = try JSONSerialization.data(
+                withJSONObject: dict, options: [.prettyPrinted, .sortedKeys]
+            )
+            try data.write(to: configPath)
+        } catch {
+            debugPrint(error)
+        }
+    }
+
+    var configPath: URL {
+        let homeDir = FileManager.default.homeDirectoryForCurrentUser
+        return homeDir.appendingPathComponent(".config/rekey/config.json", isDirectory: false)
     }
 
     private func getButton(_ direction: Direction) -> NSButton {
--- rekey/shared/AppError.swift
+++ rekey/shared/AppError.swift
@@ -1,3 +1,4 @@
 enum AppError: Error {
     case accessibility(_ message: String, _ code: Int32? = nil)
+    case config(_ message: String)
 }
