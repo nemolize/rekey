a45c0c95a1ce45ebfda7998e48733a28155e796c
nemolize <nemolize@users.noreply.github.com>
2020-05-26 01:28:22 +0900

    feat: adds Menubar icons

--- rekey/AppDelegate.swift
+++ rekey/AppDelegate.swift
@@ -11,13 +11,44 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
     let appService = AppService()
 
+    let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
+
     func applicationDidFinishLaunching(_: Notification) {
         if !appService.hasAccessibilityPermission() {
             NSApplication.shared.terminate(self)
         }
+
+        let icon = NSImage(named: NSImage.Name("MenuBarIcon"))
+        icon?.isTemplate = true
+
+        statusItem.button?.image = icon
+        constructMenu()
     }
 
     func applicationWillTerminate(_: Notification) {
         // Insert code here to tear down your application
+    }
+
+    func constructMenu() {
+        let menu = NSMenu()
+
+        menu.addItem(
+            NSMenuItem(
+                title: "Preferences",
+                // TODO: add preferences action
+                action: nil,
+                keyEquivalent: "P"
+            )
+        )
+        menu.addItem(NSMenuItem.separator())
+        menu.addItem(
+            NSMenuItem(
+                title: "Quit Rekey",
+                action: #selector(NSApplication.shared.terminate(_:)),
+                keyEquivalent: "q"
+            )
+        )
+
+        statusItem.menu = menu
     }
 }
