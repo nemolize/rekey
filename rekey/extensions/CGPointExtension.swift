e0ca747c3f9de7112b31420c192cd796365f38ae
nemolize <nemolize@users.noreply.github.com>
2020-11-25 02:09:36 +0900

    chore: remove unnecessary public statement

--- rekey/extensions/CGPointExtension.swift
+++ rekey/extensions/CGPointExtension.swift
@@ -1,34 +1,34 @@
 import Foundation
 
-extension CGPoint {
-    public var length: CGFloat { sqrt(x * x + y * y) }
+public extension CGPoint {
+    var length: CGFloat { sqrt(x * x + y * y) }
 
-    public static func += (lhs: inout CGPoint, rhs: CGPoint) {
+    static func += (lhs: inout CGPoint, rhs: CGPoint) {
         lhs.x += rhs.x
         lhs.y += rhs.y
     }
 
-    public static func + (lhs: CGPoint, rhs: CGPoint) -> CGPoint {
+    static func + (lhs: CGPoint, rhs: CGPoint) -> CGPoint {
         CGPoint(x: lhs.x + rhs.x, y: lhs.y + rhs.y)
     }
 
-    public static func - (lhs: CGPoint, rhs: CGPoint) -> CGPoint {
+    static func - (lhs: CGPoint, rhs: CGPoint) -> CGPoint {
         CGPoint(x: lhs.x - rhs.x, y: lhs.y - rhs.y)
     }
 
-    public static func + (lhs: CGPoint, rhs: CGFloat) -> CGPoint {
+    static func + (lhs: CGPoint, rhs: CGFloat) -> CGPoint {
         CGPoint(x: lhs.x + rhs, y: lhs.y + rhs)
     }
 
-    public static func - (lhs: CGPoint, rhs: CGFloat) -> CGPoint {
+    static func - (lhs: CGPoint, rhs: CGFloat) -> CGPoint {
         CGPoint(x: lhs.x - rhs, y: lhs.y - rhs)
     }
 
-    public static func * (lhs: CGPoint, rhs: CGFloat) -> CGPoint {
+    static func * (lhs: CGPoint, rhs: CGFloat) -> CGPoint {
         CGPoint(x: lhs.x * rhs, y: lhs.y * rhs)
     }
 
-    public static func / (lhs: CGPoint, rhs: CGFloat) -> CGPoint {
+    static func / (lhs: CGPoint, rhs: CGFloat) -> CGPoint {
         CGPoint(x: lhs.x / rhs, y: lhs.y / rhs)
     }
 }
